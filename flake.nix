e2a6f4d262c2457f6e0e598e4d587b099f9c7f8c
Vyas Nellutla <me@vyas-n.com>
2025-03-19 21:36:56 -0500

    feat: try nix take2

---
+++ flake.nix
@@ -0,0 +1,54 @@
+{
+  description = "My personal website used to learn and test things.";
+
+  inputs = {
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.11";
+
+    flake-compat.url = "github:edolstra/flake-compat";
+
+    crane.url = "github:ipetkov/crane";
+
+    flake-utils.url = "github:numtide/flake-utils";
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs-stable";
+    };
+  };
+
+  outputs = { self, nixpkgs-stable, crane, flake-utils, rust-overlay, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs-stable = import nixpkgs-stable {
+          inherit system;
+          overlays = [ (import rust-overlay) ];
+        };
+
+        # Use the toolchain from the `rust-toolchain.toml` file
+        rustToolchain =
+          pkgs-stable.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+        craneLib = (crane.mkLib pkgs-stable).overrideToolchain rustToolchain;
+
+      in {
+        # Development Environments
+        devShells.default = craneLib.devShell {
+          shell = pkgs-stable.nushell;
+
+          # Extra inputs can be added here;
+          # - cargo and rustc are provided by default from craneLib.
+          packages = with pkgs-stable; [
+            # IDE integrations
+            nil
+            nixfmt-classic
+            rust-analyzer
+            nodePackages.prettier
+
+            # Dev Tools
+            trunk
+            nushell
+            wrangler
+            nodePackages.nodejs
+          ];
+        };
+      });
+}
